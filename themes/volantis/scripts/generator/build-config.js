f9fc3743f2e6ff12644b7462bcc213934327ba82
blankwy <2035650646@qq.com>
2022-03-27 14:39:23 +0800

    i

---
+++ themes/volantis/scripts/generator/build-config.js
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+// 'use strict';
+
+// hexo.extend.generator.register('volantis-init-config', () => {
+//   debugger;
+//   return [
+//     { path: 'volantis-webconfig.json', data: hexo.config },
+//     { path: 'volantis-themeconfig.json', data: hexo.theme.config }
+//   ]
+// });
+=======
+// 'use strict';
+
+// hexo.extend.generator.register('volantis-init-config', () => {
+//   debugger;
+//   return [
+//     { path: 'volantis-webconfig.json', data: hexo.config },
+//     { path: 'volantis-themeconfig.json', data: hexo.theme.config }
+//   ]
+// });
+>>>>>>> c7d3d7f (e)
